2ff8bb110e081c6fa40f33ca4935322a349d2c19
Han Li <vcfvct@gmail.com>
2025-12-14 17:24:57 -0500

    fix: update contact API middleware signature

--- extensions/contact/api/contactMail/[bodyparser]mailsender.js
+++ extensions/contact/api/contactMail/[bodyparser]mailsender.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-module.exports = async (req, res, delegate, next) => {
+module.exports = async (req, res, next) => {
 	const { name, phone, email, inquiry } = req.body;
 	// Simple validation
 	if (!email || !inquiry) {
